0e264e72f21ae00520c76afc0d74cfa1f8ea7864
Amir Nasir <amirnasir0@gmail.com>
2025-10-02 14:20:19 +0530

    deoghar

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,13 @@ const nextConfig: NextConfig = {
       //   destination: "/our-clinic",
       //   permanent: true,
       // },
+      {
+        source: "/our-clinic/insono-hearing-clinic-deoghar",
+        destination: "/our-clinic/deoghar",
+        permanent: true, // 301 redirect (SEO-friendly)
+      },
+
+
        {
         source: "/blog/hearing-aid-prices-in-2025-latest-price-list-and-buying-guide",
         destination: "/hearing-aid-price",
