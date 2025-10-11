fe481eae56132cddb8084e5493e487e6423138cd
Amir Nasir <amirnasir0@gmail.com>
2025-10-12 00:13:05 +0530

    chatbotrmv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,12 +65,12 @@ export default function RootLayout({
   return (
     <html lang="en"><head>
       {/* External chatbot widget */}
-      <Script
+      {/* <Script
         src="https://chat-xbot.webspecia.in/js/widget/omf5pwsilxzzkba1/float.js"
         strategy="afterInteractive"
         async
         defer
-      />
+      /> */}
       <meta name="google-site-verification" content="_w3rNIazk1WMe-urSCcrtpzyAcqTeopxMU1qqLd0p6k" />
       </head>
       <body className="font-museo bg-white text-gray-900 antialiased bg-gradient-to-b from-[#eaf5ff] to-white">
